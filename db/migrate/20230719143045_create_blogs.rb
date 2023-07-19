30bc3016407545518d8ccf2ba18c9c07581cce17
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-07-19 23:32:01 +0900

    カラム追加

--- db/migrate/20230719143045_create_blogs.rb
+++ db/migrate/20230719143045_create_blogs.rb
@@ -1,6 +1,9 @@
 class CreateBlogs < ActiveRecord::Migration[6.0]
   def change
     create_table :blogs do |t|
+      t.string :title
+      t.text :content
+      t.datetime :start_time
 
       t.timestamps
     end
